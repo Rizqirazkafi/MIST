f2050984feb6cfee27cdb5edd8f41dc18eb7d5a6
Haris Firmansyah <acc.cp.haris@gmail.com>
2020-09-26 20:40:44 +0700

    Add main page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Main Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+  
+<div class="container">
+  <h1>Hello world!</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam diam eros, fermentum nec felis nec, congue porta justo. Fusce id cursus lectus. Phasellus sed molestie ante, id viverra ligula. Aliquam a ultrices leo. Nam viverra urna a erat rutrum, ut dignissim ligula blandit. Nullam aliquet dapibus lorem eget imperdiet. Etiam id vestibulum ipsum. Integer rhoncus malesuada justo, sed lacinia magna malesuada eget. Duis posuere erat vel turpis efficitur tempor.</p> 
+  <button type="button" class="btn btn-success">Click me!</button>
+</div>
+
+</body>
+</html>
